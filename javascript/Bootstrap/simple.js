bbfe7db50a14e39eb3709a2cfd72dcfe3fa03338
sea_dong gyun <seadonggyun@gmail.com>
2021-09-09 15:39:45 +0900

    addclass 사용한 modal view system

--- javascript/Bootstrap/simple.js
+++ javascript/Bootstrap/simple.js
@@ -1,3 +1,8 @@
+$("#show-menu").on("click", function () {
+  $(".left-menu").animate({ marginLeft: "0px" }, 500);
+});
+
+//===================================================================================================================================
 //기본js 로 모달창 띄우기
 //class 선언뒤에는 항상 위치값을 기입해야한다.
 // document
@@ -6,13 +11,28 @@
 //     document.getElementsByClassName("black-background")[0].style.display =
 //       "block";
 //   });
-//jQuery 로 모달창 띄우기
-$(".logbtn").on("click", function () {
-  $(".black-background").fadeIn();
-});
 
+//[jQuery 로 모달창 띄우기]
+// $(".logbtn").on("click", function () {
+//   $(".black-background").fadeIn();
+// });
+
+//[jQuery  css로 모달창 띄우기]
+// $(".logbtn").on("click", function () {
+//   $(".black-background").css("transform", "translateY(0px)");
+// });
+
+// $(".closebtn").on("click", function () {
+//   $(".black-background").css("transform", "translateY(-1000px)");
+// });
+
+//[jQuery  addClass로 모달창 띄우기]
+//addClass()의 파라미터에는 "."을 붙히지 않아도 된다.
+$(".logbtn").on("click", function () {
+  $(".black-background").addClass("slide-down");
+});
 $(".closebtn").on("click", function () {
-  $(".black-background").fadeOut();
+  $(".black-background").addClass("slide-up");
 });
 //===================================================================================================================================
 //기본js로 nav창 띄우기
@@ -27,20 +47,20 @@ $(".nav-sub-button").on("click", function () {
   $(".nav-sub").slideToggle();
 });
 //===================================================================================================================================
-//form 창 비워져 있으면 전송안되게 제한 하는 함수
-$("form").on("submit", function (e) {
-  if ($("#email").val() == "" && $("#password").val() == "") {
-    e.preventDefault();
-    $("#email-alert").show();
-    $("#password-alert").show();
-  } else if ($("#email").val() == "") {
-    e.preventDefault();
-    $("#email-alert").show();
-  } else if ($("#password").val() == "") {
-    e.preventDefault();
-    $("#password-alert").show();
-  }
-});
+//form 창 email,password 비워져 있으면 전송안되게 제한 하는 함수
+// $("form").on("submit", function (e) {
+//   if ($("#email").val() == "" && $("#password").val() == "") {
+//     e.preventDefault();
+//     $("#email-alert").show();
+//     $("#password-alert").show();
+//   } else if ($("#email").val() == "") {
+//     e.preventDefault();
+//     $("#email-alert").show();
+//   } else if ($("#password").val() == "") {
+//     e.preventDefault();
+//     $("#password-alert").show();
+//   }
+// });
 //===================================================================================================================================
 //삼육구게임 기본!
 // 먼저 input값과 버튼을 연결
