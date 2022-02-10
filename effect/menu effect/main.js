938eb0bf0172a456965fee3063d3a3cad5f11c91
seadonggyun4 <seadonggyun@gmail.com>
2022-02-10 12:00:00 +0900

    마우스 움직임 따라가는 effect

--- effect/menu effect/main.js
+++ effect/menu effect/main.js
@@ -21,7 +21,10 @@ function horizontalIndicator(e){
   horizontalBar.style.left = e.offsetLeft + 'px';
   horizontalBar.style.width = e.offsetWidth + 'px';
   horizontalBar.style.top = e.offsetTop + e.offsetHeight + 'px';
+  console.log(horizontalBar.style.left)
 }
+
+
 
 
 function verticalIndicator(e){
