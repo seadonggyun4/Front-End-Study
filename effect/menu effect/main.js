4169ec70c3b33091ed235571497f9c7e16f83a13
seadonggyun4 <seadonggyun@gmail.com>
2022-02-15 16:16:11 +0900

    ✅ menu underline bar effect study

---
+++ effect/menu effect/main.js
@@ -0,0 +1,31 @@
+let horizontalBar = document.getElementById('horizontal-underline');
+let horizontalMenus = document.querySelectorAll('nav:first-child a');
+let verticalBar = document.getElementById('vertical-underline');
+let verticalMenus = document.querySelectorAll('nav:nth-child(2) a');
+
+
+horizontalMenus.forEach((menu) => 
+menu.addEventListener('click', (e)=>
+  horizontalIndicator(e.currentTarget)
+  )
+);
+
+
+verticalMenus.forEach( (menu) => 
+  menu.addEventListener('click', (e) => 
+    verticalIndicator(e.currentTarget) 
+    )
+)
+
+function horizontalIndicator(e){
+  horizontalBar.style.left = e.offsetLeft + 'px';
+  horizontalBar.style.width = e.offsetWidth + 'px';
+  horizontalBar.style.top = e.offsetTop + e.offsetHeight + 'px';
+}
+
+
+function verticalIndicator(e){
+  verticalBar.style.left = e.offsetLeft + 'px';
+  verticalBar.style.width = e.offsetWidth + 'px';
+  verticalBar.style.top = e.offsetTop + e.offsetHeight + 'px';
+}
